98058c9e22c76a355eca2e5c963e12ac36a91803
Maulana <molaawoii@gmail.com>
2026-07-28 16:04:48 +0700

    feat(cards) : implement edit card title - added custom hook useModalTaskDetail and create modal component ModalTaskDetail and implement it - added handleClickTaskItem method inside custom hook useTaskSortableItem - implement ModalTaskDetail inside DetailProjectContainer - implement handleClickTaskItem inside taskSortableItem

--- src/components/pages/Projects/DetailProject/components/DetailProjectContainer.jsx
+++ src/components/pages/Projects/DetailProject/components/DetailProjectContainer.jsx
@@ -17,6 +17,7 @@ import {
 import { DRAG_CARD, DRAG_LIST } from '@/utils/constants';
 import TaskSortableItem from './TaskSortableItem';
 import ProjectInfo from './ProjectInfo';
+import ModalTaskDetail from '../../Modals/ModalTaskDetail';
 
 const DetailProjectContainer = () => {
   const {
@@ -50,64 +51,67 @@ const DetailProjectContainer = () => {
   };
 
   return (
-    <SidebarLayout
-      pageTitle={`${detailProjectData.title} (${detailProjectContext.getProjectInitials})`}
-      breadcrumbs={[
-        {
-          label: 'Daftar Proyek',
-          href: '/projects',
-        },
-        {
-          label: detailProjectData.title,
-        },
-      ]}
-    >
-      <DndContext
-        onDragStart={handleDragStart}
-        onDragEnd={handleDragEnd}
-        onDragCancel={handleDragCancel}
-        sensors={sensors}
+    <>
+      <SidebarLayout
+        pageTitle={`${detailProjectData.title} (${detailProjectContext.getProjectInitials})`}
+        breadcrumbs={[
+          {
+            label: 'Daftar Proyek',
+            href: '/projects',
+          },
+          {
+            label: detailProjectData.title,
+          },
+        ]}
       >
-        <ProjectInfo />
-        <SortableContext
-          items={boardListDataMapPublicIds}
-          strategy={horizontalListSortingStrategy}
+        <DndContext
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          onDragCancel={handleDragCancel}
+          sensors={sensors}
         >
-          <Stack
-            direction={'row'}
-            justifyContent={'flex-start'}
-            alignContent={'flex-start'}
-            gap={2}
-            pb={5}
-            sx={{
-              overflowX: 'auto',
+          <ProjectInfo />
+          <SortableContext
+            items={boardListDataMapPublicIds}
+            strategy={horizontalListSortingStrategy}
+          >
+            <Stack
+              direction={'row'}
+              justifyContent={'flex-start'}
+              alignContent={'flex-start'}
+              gap={2}
+              pb={5}
+              sx={{
+                overflowX: 'auto',
+              }}
+            >
+              {boardListData?.map((item) => (
+                <ListSortableItem
+                  key={item.public_id}
+                  id={item.public_id}
+                  item={item}
+                />
+              ))}
+              <CreateNewList />
+            </Stack>
+          </SortableContext>
+          <DragOverlay
+            dropAnimation={{
+              sideEffects: defaultDropAnimationSideEffects({
+                styles: {
+                  active: {
+                    opacity: '0.4',
+                  },
+                },
+              }),
             }}
           >
-            {boardListData?.map((item) => (
-              <ListSortableItem
-                key={item.public_id}
-                id={item.public_id}
-                item={item}
-              />
-            ))}
-            <CreateNewList />
-          </Stack>
-        </SortableContext>
-        <DragOverlay
-          dropAnimation={{
-            sideEffects: defaultDropAnimationSideEffects({
-              styles: {
-                active: {
-                  opacity: '0.4',
-                },
-              },
-            }),
-          }}
-        >
-          {renderDragOverlay()}
-        </DragOverlay>
-      </DndContext>
-    </SidebarLayout>
+            {renderDragOverlay()}
+          </DragOverlay>
+        </DndContext>
+      </SidebarLayout>
+      <ModalTaskDetail />
+    </>
   );
 };
 
--- src/components/pages/Projects/DetailProject/hooks/useTaskSortableItem.js
+++ src/components/pages/Projects/DetailProject/hooks/useTaskSortableItem.js
@@ -1,8 +1,10 @@
 import { useSortable } from '@dnd-kit/sortable';
 import useDetailProjectContext from './useDetailProjectContext';
 import { DRAG_CARD } from '@/utils/constants';
+import { useSearchParams } from 'react-router';
 
 const useTaskSortableItem = ({ id, item, listId }) => {
+  const [, setSearchParams] = useSearchParams();
   const detailProjectContext = useDetailProjectContext();
 
   const {
@@ -20,6 +22,15 @@ const useTaskSortableItem = ({ id, item, listId }) => {
     },
   });
 
+  const handleClickTaskItem = (e) => {
+    e.stopPropagation();
+    detailProjectContext.setIsOpenTaskDetail(true);
+    setSearchParams({
+      taskId: item.public_id,
+      listId,
+    });
+  };
+
   return {
     detailProjectContext,
     isDragging,
@@ -28,6 +39,7 @@ const useTaskSortableItem = ({ id, item, listId }) => {
     setNodeRef,
     listeners,
     transition,
+    handleClickTaskItem,
   };
 };
 
