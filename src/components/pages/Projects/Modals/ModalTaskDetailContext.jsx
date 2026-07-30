1d07021fbc8e5029808d9a0fea76634db89c569e
Maulana <molaawoii@gmail.com>
2026-07-30 22:37:02 +0700

    refactor(modal-task-detail): centralize state and logic using Context API

---
+++ src/components/pages/Projects/Modals/ModalTaskDetailContext.jsx
@@ -0,0 +1,71 @@
+import services from "@/services";
+import { createContext, useEffect, useState } from "react";
+import { useLoaderData, useSearchParams } from "react-router";
+
+const defaultState = {
+    taskDetailData: {},
+    setTaskDetailData() {},
+    taskId: '',
+    listId: '',
+    membersData: [],
+    setMembersData() {},
+    async fetchTaskDetail() {},
+    async fetchProjectMembers() {},
+};
+
+export const ModalTaskDetailContext = createContext(defaultState);
+
+const ModalTaskDetailProveder = ({children}) => {
+    const detailProjectData = useLoaderData();
+
+    const [searchParams] = useSearchParams();
+
+    const [taskDetailData, setTaskDetailData] = useState({});
+
+    const [membersData, setMembersData] = useState({});
+
+    const boardId = detailProjectData.public_id;
+
+    const taskId = searchParams.get('taskId');
+
+    const listId = searchParams.get('listId');
+
+    const fetchTaskDetail = async (taskId) => {
+        const response = await services.cards.getDetail(taskId);
+
+        setTaskDetailData(response.data.data);
+    };
+
+    const fetchProjectMemebers = async (boardId) => {
+        const response = await services.boards.getMembers(boardId);
+
+        setMembersData(response.data.data);
+    };
+
+    const initTaskDetail = async () => {
+        await fetchTaskDetail(taskId);
+
+        await fetchProjectMemebers(boardId);
+    };
+
+    useEffect(() => {
+        if (taskId && listId && boardId) {
+            initTaskDetail();
+        }
+    }, [taskId, listId, boardId]);
+
+    return (
+        <ModalTaskDetailContext.Provider value={{ 
+            taskId,
+            listId,
+            taskDetailData,
+            fetchTaskDetail,
+            fetchProjectMemebers,
+            membersData,
+         }}>
+            {children}
+        </ModalTaskDetailContext.Provider>
+    )
+}
+
+export default ModalTaskDetailProveder;
--- src/components/pages/Projects/Modals/hooks/useModalTaskDetail.js
+++ src/components/pages/Projects/Modals/hooks/useModalTaskDetail.js
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 import { useLoaderData, useSearchParams } from 'react-router';
 import useDetailProjectContext from '../../DetailProject/hooks/useDetailProjectContext';
 import services from '@/services';
 import { useForm } from 'react-hook-form';
-import dayjs from 'dayjs';
+import useModalTaskDetailContext from './useModalTaskDetailContext';
 
 const useModalTaskDetail = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const [taskDetailData, setTaskDetailData] = useState({});
+  const [ _, setSearchParams] = useSearchParams();
 
   const [isLoading, setLoading] = useState(false);
 
@@ -18,25 +16,17 @@ const useModalTaskDetail = () => {
 
   const [editDueDate, setEditDueDate] = useState(false);
 
-  const [editAssignee, setEditAssignee] = useState(false);
-
   const [isShowConfirmDelete, setShowConfirmDelete] = useState(false);
 
   const detailProjectData = useLoaderData();
 
   const detailProjectContext = useDetailProjectContext();
 
-  const taskId = searchParams.get('taskId');
+  const modalTaskDetailContext = useModalTaskDetailContext();
 
-  const listId = searchParams.get('listId');
+const { taskId, listId, taskDetailData, fetchTaskDetail } = modalTaskDetailContext;
 
   const formTask = useForm();
-
-  const fetchTaskDetail = async (taskId) => {
-    const response = await services.cards.getDetail(taskId);
-
-    setTaskDetailData(response.data.data);
-  };
 
   const onSubmit = async (values) => {
     setLoading(true);
@@ -67,33 +57,21 @@ const useModalTaskDetail = () => {
     await detailProjectContext.fetchBoardLists();
   };
 
-  useEffect(() => {
-    if (taskId && listId) {
-      fetchTaskDetail(taskId);
-    }
-  }, [taskId, listId]);
-
   return {
-    searchParams,
     taskDetailData,
     isLoading,
     editDescription,
     editTitle,
     editDueDate,
-    editAssignee,
     isShowConfirmDelete,
 
-    setSearchParams,
-    setTaskDetailData,
     setLoading,
     setEditDescription,
     setEditTitle,
     setEditDueDate,
-    setEditAssignee,
     setShowConfirmDelete,
 
     detailProjectData,
-    detailProjectContext,
     taskId,
     listId,
     formTask,
